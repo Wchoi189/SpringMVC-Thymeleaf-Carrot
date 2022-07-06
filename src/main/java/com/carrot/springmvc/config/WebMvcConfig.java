19c12281b5374107138d56cd828ade1deaa7a385
Woong Choi <97170457+Wchoi189@users.noreply.github.com>
2022-07-06 14:19:50 +0900

    Setup files. Thymeleaf. Folder structures defined.

---
+++ src/main/java/com/carrot/springmvc/config/WebMvcConfig.java
@@ -0,0 +1,72 @@
+package com.carrot.springmvc.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.carrot.springmvc.app.board.controller")
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    private ApplicationContext applicationContext;
+    // + JSP View Resolver
+    @Bean
+    public InternalResourceViewResolver jspViewResolver(){
+        final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/jsp/");
+        viewResolver.setViewNames("*.jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public ViewResolver thymeleafResolver() {
+        ThymeleafViewResolver ivr = new ThymeleafViewResolver();
+        ivr.setTemplateEngine(templateEngine());
+        ivr.setOrder(0);
+        return ivr;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver srtr = new SpringResourceTemplateResolver();
+        srtr.setApplicationContext(applicationContext);
+        srtr.setPrefix("/WEB-INF/views/html/");
+        srtr.setSuffix(".html");
+        return srtr;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+        // Static Resources 설정
+        registry.addResourceHandler("/**","/resources/**")
+                .addResourceLocations("classpath:/static/**/*","/resources/**/*")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+    }
+
+}
